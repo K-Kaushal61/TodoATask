43c820b9773a792e4354a8cc3b312c5e14fb6a00
K-Kaushal61 <kartikkaushal6261@gmail.com>
2026-01-23 02:18:30 +0530

    Fix repo root to parent folder

---
+++ client/src/components/home/Home.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import '../../index.css';
+
+const Home = () => {
+  return (
+    <div class='Main-div flex justify-around gap-10 p-5 h-full w-screen'>
+        <div class="todo-div bg-red-400 flex flex-col min-h-50 max-h-screen w-screen p-3 rounded-xl">
+            <h1 class='text-2xl font-semibold '>To-Do</h1>
+            <hr class='bg-black mt-1 mb-2'/>
+            <div class="todotask flex flex-col bg-black p-2 gap-2 rounded-xl h-full">
+                <div class='taskheading'>
+                    <input class=' text-white placeholder:text-gray-300 outline-0' placeholder='Heading' type='text'/>
+                </div>
+                <div class='taskdescription flex flex-wrap'>
+                    <textarea wrap='soft' maxlength={120} class='text-white resize-none placeholder:text-gray-300 outline-0 w-full ' placeholder='Description...'/>
+                </div>
+            </div>
+        </div>
+
+        <div class="todo-div bg-green-400 flex flex-col min-h-50 max-h-screen w-screen p-3 rounded-xl">
+            <h1 class='text-2xl font-semibold '>To-Do</h1>
+            <hr class='bg-black mt-1 mb-2'/>
+            <div class="todotask flex flex-col bg-black p-2 gap-2 rounded-xl h-full">
+                <div class='taskheading'>
+                    <input class=' text-white placeholder:text-gray-300 outline-0' placeholder='Heading' type='text'/>
+                </div>
+                <div class='taskdescription flex flex-wrap'>
+                    <textarea wrap='soft' maxlength={120} class='text-white resize-none placeholder:text-gray-300 outline-0 w-full ' placeholder='Description...'/>
+                </div>
+            </div>
+        </div>
+
+        <div class="todo-div bg-blue-400 flex flex-col min-h-50 max-h-screen w-screen p-3 rounded-xl">
+            <h1 class='text-2xl font-semibold '>To-Do</h1>
+            <hr class='bg-black mt-1 mb-2'/>
+            <div class="todotask flex flex-col bg-black p-2 gap-2 rounded-xl h-full">
+                <div class='taskheading'>
+                    <input class=' text-white placeholder:text-gray-300 outline-0' placeholder='Heading' type='text'/>
+                </div>
+                <div class='taskdescription flex flex-wrap'>
+                    <textarea wrap='soft' maxlength={120} class='text-white resize-none placeholder:text-gray-300 outline-0 w-full ' placeholder='Description...'/>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
